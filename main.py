60fc0462adf67113cbc393e914c50f46b3fc5795
Ralfs Jurjanis <97153972+projurjanis@users.noreply.github.com>
2022-02-16 22:23:23 +0000

    iesaku darbu

---
+++ main.py
@@ -0,0 +1,10 @@
+max_ietilpiba=20
+restart="y"
+
+while restart == "y":
+  izvele=int(input("1.Veikt rezervāciju\n2.Atcelt rezervāciju\n3.Pamest lietotni\nIevadi skaitli!\n"))
+  if izvele==3:
+    restart="n"
+  if izvele==1:
+    
+
